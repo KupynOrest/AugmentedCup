e7ef8bb554977837242e2647169af1d8963d3490
alexsosn <alex.sosn88@gmail.com>
2017-01-21 13:44:06 +0200

    texture provider

---
+++ AugmentedCup/TextureProvider.h
@@ -0,0 +1,15 @@
+//
+//  TextureProvider.h
+//  AugmentedCup
+//
+//  Created by Tanya on 1/21/17.
+//  Copyright © 2017 kupynвkupyn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TextureProvider : NSObject
+
+
+
+@end
---
+++ AugmentedCup/TextureProvider.m
@@ -0,0 +1,13 @@
+//
+//  TextureProvider.m
+//  AugmentedCup
+//
+//  Created by Tanya on 1/21/17.
+//  Copyright © 2017 kupynвkupyn. All rights reserved.
+//
+
+#import "TextureProvider.h"
+
+@implementation TextureProvider
+
+@end
